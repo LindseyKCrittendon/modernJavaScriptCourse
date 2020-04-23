14102a1d6a113576315a655ddc07e002813875b6
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-04-23 16:04:35 -0400

    refresher on arrays in section 2

---
+++ script.js
@@ -0,0 +1,141 @@
+// const firstName = 'William';
+// const lastName = 'Johnson';
+// const age = 36;
+// const str = 'Hello there my name is Brad';
+
+// let val;
+
+// val = firstName + lastName;
+// //concatenation
+// val = firstName + ' ' + lastName
+// //adds a space
+// //append
+// val = 'Brad';
+// val += 'Trebble';
+// val = 'Hello, my name is ' + firstName+ 'and I am ' + age;
+// //escaping
+// val = 'That\'s awesome, I can\'t wait'
+//length
+// val = firstName.length;
+// //concat
+// val = firstName.concat(' ', lastName);
+// //change case
+// val = firstName.toUpperCase();
+// val = firstName.toLowerCase();
+
+// val = firstName[0];
+
+// //indexOf()
+// val = firstName.indexOf('l');
+// val = firstName.lastIndexOf('l');
+
+// //charAt()
+// val = firstName.charAt('2');
+//get last character
+// val = firstName.charAt(firstName.length - 1);
+// //substring()
+// val = firstName.substring(0, 4);
+// //slice
+// val = firstName.slice(-3);
+// //split
+// val = str.split(' ');
+// //replace
+// val = str.replace('Brad', 'Jack');
+// //includes//returns true/false statement
+// val = str.includes('Hello');
+
+// console.log(val)
+
+//template literals
+// const name = 'John';
+// const age = 31;
+// const job = 'Web Developer';
+// const city = 'Miami';
+
+//without template strings (es5)
+
+// html = '<ul><li>Name: ' + name + '</li><li>Age: ' + age + ' </li><li>Job: ' + job + '</li><li>City: '+ city +' </li></ul>';
+
+// document.body.innerHTML = html;
+
+// //with template strings(es6) or template literals - cleaner way
+// html = 
+// `<ul>
+//     <li>${name}</li>
+//     <li>${age}</li>
+//     <li>${job}</li>
+//     <li>${city}</li>
+//     <li>${2 + 2}</li>
+   
+//     <li>${age > 30 ? 'Over 30' : 'Under 30'}</li>
+// </ul>`;
+// document.body.innerHTML = html;
+
+//create some arrays
+const numbers = [43, 56, 33, 23, 44, 36, 5];
+const numbers2 = new Array(22,45, 33,76,54);
+const fruit = ['apple', 'banana', 'orange', 'pear'];
+const mixed = [22, 'hello', true, undefined, null, {a:1, b:1}, new Date()];
+// console.log(mixed);
+
+let val;
+//get array length
+val = numbers.length;
+//lets you know if you are actually working with an array.
+val = Array.isArray(numbers);
+val = Array.isArray('hello');
+
+//get a single value
+
+val = numbers[3];
+val = numbers[0];
+//insert into array
+numbers[2] = 100;
+//find index of value
+val = numbers.indexOf(36);
+
+//MUTATING ARRAYS
+//add onto end
+// numbers.push(250);
+// //add onto front
+// numbers.unshift(120);
+// //take off from end
+// numbers.pop();
+// //take off from front
+// numbers.shift();
+// //splice values
+// numbers.splice(1, 1);
+// //reverse
+// numbers.reverse();
+
+// //concatenate arrays
+// val = numbers.concat(numbers2);
+
+//sort
+// val = fruit.sort();
+// val = numbers.sort();
+
+// //use the compare function
+// val = numbers.sort(function(x, y){
+//     return x-y;
+// });
+
+// //to reverse that direction
+// val = numbers.sort(function(x, y){
+// return y-x;
+// });
+
+//find
+function under50(num){
+    return num < 50;
+}
+//found first number under 50
+val = numbers.find(under50)
+
+function over50(num){
+    return num > 50;
+}
+val = numbers.find(over50);
+
+console.log(numbers);
+console.log(val);
