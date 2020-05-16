063534f2b8f7a4099f4fc89037d93e76338f32b7
Lindsey Crittendon <LindseyKCrittendon@gmail.com>
2020-05-16 04:56:45 -0400

    traversing the dom

--- app.js
+++ app.js
@@ -39,3 +39,62 @@
 
 
 // console.log(val);
+
+//document.getElementById()
+
+// console.log(document.getElementById('task-title'))
+
+//get things from the element
+
+// console.log(document.getElementById("task-title").id);
+
+// console.log(document.getElementById("task-title").className);
+
+//change styling
+// document.getElementById("task-title").style.background = "black";
+// document.getElementById("task-title").style.color = "white";
+// document.getElementById("task-title").style.padding = "5px";
+
+//change content
+// document.getElementById("task-title").textContent = "Task List";
+// document.getElementById("task-title").innerText = "My Tasks";
+// document.getElementById("task-title").innerHTML = "<span style ='color:red'>Task List</span>";
+
+//shouldn't copy and paste.  Change the repeating text into variable
+
+// const taskTitle = document.getElementById("task-title");
+
+// taskTitle.textContent = "My Tasks";
+// taskTitle.innerText = "My TAsks";
+//you get the idea
+
+//document.querySelector() - we an select anything with this and very powerful
+
+// console.log(document.querySelector("#task-title"));
+
+// document.querySelector("li").style.color = "red"; //single element selector, and it only selects the first one
+
+// document.querySelector("ul li").style.color = "blue";
+
+// document.querySelector("li:last-child").style.color = "blue";
+
+// document.querySelector("li:nth-child(3)").style.color = "green";
+
+// document.querySelector("li:nth-child(4)").textContent = "Hello World";
+
+//document.getElementsByClassName
+
+// const items = document.getElementsByClassName("collection-item");
+
+// console.log(items);
+
+// console.log(items[0]);
+
+// items[0].style.color = "red";
+// items[3].textContent = "Hello";
+
+// const listItems = document.querySelector("ul").getElementsByClassName("collection-item");
+
+// console.log(listItems)
+
+
